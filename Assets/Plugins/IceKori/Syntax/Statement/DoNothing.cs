3c6733a055727e8a986685bf5b8003fd99ea370d
molingyu <z1522716486@hotmail.com>
2018-06-26 23:58:56 +0800

    feat: add do nothing node

---
+++ Assets/Plugins/IceKori/Syntax/Statement/DoNothing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Plugins.IceKori.Syntax.Statement
+{
+    class DoNothing : BaseStatement
+    {
+        public new bool Reducible = false;
+
+        public override BaseStatement Reduce(Enviroment env)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
