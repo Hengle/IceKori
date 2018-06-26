69ddaec3a039140ae1ef1d9685c79235158c7b60
molingyu <z1522716486@hotmail.com>
2018-06-26 23:58:29 +0800

    feat: variable access

---
+++ Assets/Plugins/IceKori/Syntax/Expression/GlobalVariableGet.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Plugins.IceKori.Syntax.Error;
+
+namespace Assets.Plugins.IceKori.Syntax.Expression
+{
+    [System.Serializable]
+    public class GlobalVariableGet : BaseExpression
+    {
+        public string Name;
+
+        public GlobalVariableGet()
+        {
+
+        }
+
+        public GlobalVariableGet(string name)
+        {
+            Name = name;
+        }
+
+        public override BaseExpression Reduce(Enviroment env)
+        {
+            if (!env.ContainsKey(Name)) return new ReferenceError($"Global variable {Name} is not defined");
+            return env[Name];
+        }
+    }
+}
---
+++ Assets/Plugins/IceKori/Syntax/Expression/VariableGet.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Plugins.IceKori.Syntax.Error;
+
+namespace Assets.Plugins.IceKori.Syntax.Expression
+{
+    [System.Serializable]
+    public class VariableGet : BaseExpression
+    {
+        public string Name;
+
+        public VariableGet()
+        {
+
+        }
+
+        public VariableGet(string name)
+        {
+            Name = name;
+        }
+
+        public override BaseExpression Reduce(Enviroment env)
+        {
+            if(!env.ContainsKey(Name)) return new ReferenceError($"{Name} is not defined");
+            return env[Name];
+        }
+    }
+}
