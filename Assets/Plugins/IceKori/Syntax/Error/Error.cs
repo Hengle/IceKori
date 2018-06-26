44a18a2afeebc65f5782f9a6649a3e87a3420cda
molingyu <z1522716486@hotmail.com>
2018-06-26 20:18:25 +0800

    feat: add error node

---
+++ Assets/Plugins/IceKori/Syntax/Error/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Plugins.IceKori.Syntax.Expression;
+
+namespace Assets.Plugins.IceKori.Syntax.Error
+{
+     public abstract class Error : BaseExpression
+    {
+    }
+}
---
+++ Assets/Plugins/IceKori/Syntax/Error/TypeError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Plugins.IceKori.Syntax.Expression;
+
+namespace Assets.Plugins.IceKori.Syntax.Error
+{
+    public class TypeError : Error
+    {
+        public override BaseExpression Reduce(Enviroment env)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
