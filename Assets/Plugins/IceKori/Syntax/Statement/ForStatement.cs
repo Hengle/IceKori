04c2d7083cd22a8baffbd7fe95680e9bf44c64f7
molingyu <z1522716486@hotmail.com>
2018-06-27 00:01:35 +0800

    feat: add `for` loop node

---
+++ Assets/Plugins/IceKori/Syntax/Statement/ForStatement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Plugins.IceKori.Syntax.BaseType;
+using Assets.Plugins.IceKori.Syntax.Expression;
+using Sirenix.OdinInspector;
+
+namespace Assets.Plugins.IceKori.Syntax.Statement
+{
+    [System.Serializable]
+    public class ForStatement : BaseStatement
+    {
+        public IceKoriInt Count = new IceKoriInt();
+        public List<BaseStatement> Body = new List<BaseStatement>();
+        [HideInEditorMode]
+        public int Index;
+
+        public override BaseStatement Reduce(Enviroment env)
+        {
+            Index += 1;
+            var condition = new BinaryExpression(BinaryOperator.LessEqual, new IceKoriInt(Index), Count);
+            return new IfStatement(condition, new List<BaseStatement>(Body) {this}, new List<BaseStatement>());
+        }
+    }
+}
