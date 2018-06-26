a1d8ba46345ea969bfa7603efbd63c02b2ea7b67
molingyu <z1522716486@hotmail.com>
2018-06-26 20:20:10 +0800

    feat: add if && throw statement

---
+++ Assets/Plugins/IceKori/Syntax/Statement/IfStatement.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Plugins.IceKori.Syntax.BaseType;
+using Assets.Plugins.IceKori.Syntax.Expression;
+using System.Collections.Generic;
+using Assets.Plugins.IceKori.Syntax.Error;
+
+namespace Assets.Plugins.IceKori.Syntax.Statement
+{
+    [System.Serializable]
+    public class IfStatement : BaseStatement
+    {
+
+        public IfStatement()
+        {
+        }
+
+        public IfStatement(BaseExpression condition, List<BaseStatement> consequence, List<BaseStatement> alternative)
+        {
+            Condition = condition;
+            Consequence = consequence;
+            Alternative = alternative;
+        }
+
+        public BaseExpression Condition;
+        public List<BaseStatement> Consequence = new List<BaseStatement>();
+        public List<BaseStatement> Alternative = new List<BaseStatement>();
+
+        public override BaseStatement Reduce(Enviroment env)
+        {
+            if (Condition.Reducible) return new IfStatement(Condition.Reduce(env), Consequence, Alternative);
+            if (Condition.GetType() == typeof(IceKoriBool))
+            {
+                var context = (bool)((IceKoriBool)Condition).Unbox() ? Consequence : Alternative;
+                if (context.Count == 0)
+                {
+                    return new DoNothing();
+                }
+                return new SequenceStatment(context);
+            }
+            return new Throw(new TypeError());
+        }
+    }
+}
---
+++ Assets/Plugins/IceKori/Syntax/Statement/ThrowStatment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Assets.Plugins.IceKori.Syntax.Error;
+
+namespace Assets.Plugins.IceKori.Syntax.Statement
+{
+    public class Throw : BaseStatement
+    {
+        public Error.Error Error;
+
+        public Throw()
+        {
+            Reducible = false;
+        }
+
+        public Throw(Error.Error error)
+        {
+            Reducible = false;
+            Error = error;
+        }
+
+        public override BaseStatement Reduce(Enviroment env)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
