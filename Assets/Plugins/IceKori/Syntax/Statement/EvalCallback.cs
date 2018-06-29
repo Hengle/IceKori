2c264ea831411fad5a38cbefd0d57163b4a76051
molingyu <z1522716486@hotmail.com>
2018-06-29 17:24:47 +0800

    feat: add EvalCallback node

---
+++ Assets/Plugins/IceKori/Syntax/Statement/EvalCallback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Plugins.IceKori.Syntax.Statement
+{
+    public class EvalCallback : BaseStatement
+    {
+        public Action<Enviroment, ErrorHandling> Func;
+
+        public EvalCallback(Action<Enviroment, ErrorHandling> func)
+        {
+            Reducible = true;
+            Func = func;
+        }
+        public override object[] Reduce(Enviroment env, ErrorHandling errorHandling)
+        {
+            Func(env, errorHandling);
+            return new object[]{ new DoNothing(), env, errorHandling };
+        }
+    }
+}
