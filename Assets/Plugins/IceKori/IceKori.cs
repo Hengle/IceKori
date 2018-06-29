ce90eccfee41b58491500dcf99503af51ce1a698
molingyu <z1522716486@hotmail.com>
2018-06-29 17:26:22 +0800

    feat: add global config

---
+++ Assets/Plugins/IceKori/IceKori.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Plugins.IceKori.Syntax;
+using UnityEngine;
+
+namespace Assets.Plugins.IceKori
+{
+    public static class IceKori
+    {
+
+        public static GlobalConf Conf;
+
+        public static void LoadGlobalConf()
+        {
+            var db = Resources.Load<GlobalConf>("GlobalConf");
+            db.hideFlags = HideFlags.DontSaveInBuild;
+            Conf = db;
+        }
+
+        public static void DefineGlobal()
+        {
+
+        }
+
+        public static void Init()
+        {
+            LoadGlobalConf();
+            DefineGlobal();
+        }
+    }
+}
