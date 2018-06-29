afab174c78cd412a986ba01cab1919913dbe9788
molingyu <z1522716486@hotmail.com>
2018-06-29 17:15:17 +0800

    refactor: rename

---
+++ Assets/Plugins/IceKori/Syntax/Error/BaseError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Plugins.IceKori.Syntax.Expression;
+
+namespace Assets.Plugins.IceKori.Syntax.Error
+{
+     public abstract class BaseError : BaseExpression
+     {
+         public string Name;
+         public string Msg;
+
+         public override BaseExpression Reduce(Enviroment env)
+         {
+             throw new Exception(ToString());
+         }
+     }
+}
