973259ac63a2cbd409ff1f973043740d58dce930
molingyu <z1522716486@hotmail.com>
2018-06-29 17:24:24 +0800

    feat: add Try-catch statement

---
+++ Assets/Plugins/IceKori/Syntax/Statement/TryCatch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Plugins.IceKori.Syntax.Statement
+{
+    [System.Serializable]
+    public enum ErrorType
+    {
+        All,
+        TypeError,
+        ReferenceError
+    }
+
+    [System.Serializable]
+    public class TryCatch : BaseStatement
+    {
+        public List<BaseStatement> Body;
+        public ErrorType Catch;
+        public List<BaseStatement> Rescue;
+
+        public TryCatch()
+        {
+            Reducible = true;
+        }
+
+        public TryCatch(List<BaseStatement> body, ErrorType catchType, List<BaseStatement> rescue)
+        {
+            Reducible = true;
+            Body = body;
+            Catch = catchType;
+            Rescue = rescue;
+        }
+
+        public override string ToString()
+        {
+            var str = "try{\n";
+            foreach (var baseStatement in Body)
+            {
+                str += $"{baseStatement}\n";
+            }
+
+            str += $"}} catch({Catch}) {{\n";
+            foreach (var baseStatement in Rescue)
+            {
+                str += $"{baseStatement}\n";
+            }
+            return str + "}";
+        }
+
+        public override object[] Reduce(Enviroment env, ErrorHandling errorHandling)
+        {
+            errorHandling.TryCatchStack.Push(this);
+            Body.Add(new EvalCallback((enviroment, handling) => handling.TryCatchStack.Pop()));
+            return new object[]{ new Sequence(Body), env, errorHandling};
+        }
+    }
+}
