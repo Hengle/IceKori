4e1767f1792788357c30699a77692749249a3ada
molingyu <z1522716486@hotmail.com>
2018-07-02 16:39:34 +0800

    refactor: change GlobalDefine reduce error message

--- Assets/Plugins/IceKori/Syntax/Statement/GlobalDefine.cs
+++ Assets/Plugins/IceKori/Syntax/Statement/GlobalDefine.cs
@@ -32,7 +32,7 @@ namespace Assets.Plugins.IceKori.Syntax.Statement
             if (Value.Reducible) return new object[]{ new GlobalDefine(Name, Value.Reduce(env)), env, errorHandling };
             if (env.GlobalVariables.ContainsKey(Name))
             {
-                return new object[]{ new Throw(new TypeError($"Identifier \"{Name}\" has already been declared")), env, errorHandling };
+                return new object[]{ new Throw(new TypeError($"Global identifier \"{Name}\" has already been declared")), env, errorHandling };
             }
             env.GlobalVariables.Add(Name, (IceKoriBaseType)Value);
             return new object[] { new DoNothing(), env, errorHandling };
