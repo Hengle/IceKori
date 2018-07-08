b1fd13b06ebc28170324c163dfb3b3b22a431e8b
molingyu <z1522716486@hotmail.com>
2018-07-08 13:37:01 +0800

    feat: add `IceKoriDateTime` type node

---
+++ Assets/Plugins/IceKori/Syntax/BaseType/Object/IceKoriDataTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Plugins.IceKori.Syntax.BaseType.Object
+{
+    public class IceKoriDataTime : IceKoriBaseType
+    {
+        public DateTime Value;
+
+        public IceKoriDataTime()
+        {
+            Reducible = false;
+        }
+
+        public IceKoriDataTime(DateTime value)
+        {
+            Reducible = false;
+            Value = value;
+        }
+
+        public override object Unbox()
+        {
+            return Value;
+        }
+    }
+}
